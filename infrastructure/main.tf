02cc2ca41f702c34e8c8f84364ea9be36663c246
briandenicola <brian@briandenicola.com>
2024-08-07 12:47:11 -0500

    🏒 Wed Aug  7 12:47:11 CDT 2024

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,29 +3,35 @@ resource "random_id" "this" {
 }
 
 resource "random_pet" "this" {
-  length = 1
-  separator  = ""
+  length    = 1
+  separator = ""
 }
 
 resource "random_password" "password" {
-  length = 25
+  length  = 25
   special = true
 }
 
 locals {
-    location                    = var.region
-    resource_name               = "${random_pet.this.id}-${random_id.this.dec}"
+  location                        = var.region
+  resource_name                   = "${random_pet.this.id}-${random_id.this.dec}"
+  openai_name                     = "${local.resource_name}-openai"
+  kv_name                         = "${local.resource_name}-kv"
+  acr_name                        = "${replace(local.resource_name, "-", "")}acr"
+  storage_account_name            = "${replace(local.resource_name, "-", "")}sa"
+  appinsights_name                = "${local.resource_name}-appinsights"
+  loganalytics_name               = "${local.resource_name}-logs"
+  machine_learning_workspace_name = "${local.resource_name}-ml"
 }
 
-
 resource "azurerm_resource_group" "this" {
-  name                  = "${local.resource_name}_rg"
-  location              = local.location
-  
-  tags     = {
+  name     = "${local.resource_name}_rg"
+  location = local.location
+
+  tags = {
     Application = var.tags
     Components  = ""
     DeployedOn  = timestamp()
-    Deployer    = data.azurerm_client_config.current.principal_id
+    Deployer    = data.azurerm_client_config.current.object_id
   }
 }
---
+++ infrastructure/storage.tf
@@ -0,0 +1,10 @@
+resource "azurerm_storage_account" "this" {
+  name                      = local.storage_account_name
+  resource_group_name       = azurerm_resource_group.this.name
+  location                  = azurerm_resource_group.this.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  account_kind              = "StorageV2"
+  enable_https_traffic_only = true
+  min_tls_version           = "TLS1_2"
+}
