5524859c78631c985f4d38bdc5c0f7ce7d3790f4
briandenicola <brian@briandenicola.com>
2024-09-11 16:00:37 -0500

    🙏 Wed Sep 11 16:00:37 CDT 2024

--- src/autogen_example/main.py
+++ src/autogen_example/main.py
@@ -16,7 +16,9 @@ def chat() -> str:
 
     romulus = ConversableAgent(
         "romulus",
-        system_message="Your name is Romulus and you are a twin with your brother Remus.  You are arguing over which of the seven hills your new city will be founded on. You are the wiser of the two.",
+        system_message="Your name is Romulus and you are a twin with your brother Remus.  
+        "You are arguing over which of the seven hills your new city will be founded on. 
+        "You are the wiser of the two.",
         llm_config={"config_list": [
             {
                 "model": "gpt-35-turbo",
@@ -26,12 +28,14 @@ def chat() -> str:
                 "api_version": "2024-05-01-preview" 
             }     
         ]},
-        human_input_mode="NEVER",  # Never ask for human input.
+        human_input_mode="NEVER",
     )
 
     remus = ConversableAgent(
         "remus",
-        system_message="Your name is Remus and you are a twin with your brother Romulus.  You are arguing over which of the seven hills your new city will be founded on.  You are the older brother and more king like.",
+        system_message="Your name is Remus and you are a twin with your brother Romulus.  
+        "You are arguing over which of the seven hills your new city will be founded on.  "
+        "You are the older brother and more king like.",
         llm_config={"config_list": [
             {
                 "model": "gpt-35-turbo",
@@ -41,10 +45,10 @@ def chat() -> str:
                 "api_version": "2024-05-01-preview" 
             }     
         ]},
-        human_input_mode="NEVER",  # Never ask for human input.
+        human_input_mode="NEVER",
     )
-
-    result = romulus.initiate_chat(remus, message="Remus, let's start a city one of these 7 hills - Aventine, Caelian, Capitoline, Esquiline, Palatine, Quirinal, Viminal. Which one should we found it on.", max_turns=4)
+    msg = "Remus, let's start a city one of these 7 hills - Aventine, Caelian, Capitoline, Esquiline, Palatine, Quirinal, Viminal. Which one should we found it on."
+    result = romulus.initiate_chat(remus, message=msg, max_turns=4)
 
 
 if __name__ == "__main__":
