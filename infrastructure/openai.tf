9487a497e3606536fce3b432085dc481c8626084
briandenicola <brian@briandenicola.com>
2024-09-13 10:15:22 -0500

    🎲 Fri Sep 13 10:15:22 CDT 2024

--- infrastructure/openai.tf
+++ infrastructure/openai.tf
@@ -1,8 +1,9 @@
 resource "azurerm_cognitive_account" "this" {
-  name                = local.openai_name
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-  kind                = "OpenAI"
+  name                  = local.openai_name
+  resource_group_name   = azurerm_resource_group.this.name
+  location              = azurerm_resource_group.this.location
+  kind                  = "OpenAI"
+  custom_subdomain_name = local.openai_name
 
   sku_name = "S0"
 }
