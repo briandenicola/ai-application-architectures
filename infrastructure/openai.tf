3b05d8cb7a7ecebf303066fcfc60afd018ab8a13
briandenicola <brian@briandenicola.com>
2024-09-16 10:10:50 -0500

    ⛸️ Mon Sep 16 10:10:50 CDT 2024

--- infrastructure/openai.tf
+++ infrastructure/openai.tf
@@ -7,3 +7,28 @@ resource "azurerm_cognitive_account" "this" {
 
   sku_name = "S0"
 }
+
+data "azurerm_monitor_diagnostic_categories" "this" {
+  resource_id = azurerm_cognitive_account.this.id
+}
+
+resource "azurerm_monitor_diagnostic_setting" "this" {
+  depends_on = [ 
+    data.azurerm_monitor_diagnostic_categories.this 
+  ]
+  name               = "diag"
+  target_resource_id = azurerm_cognitive_account.this.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.this.id
+
+  dynamic "enabled_log" {
+    for_each = toset(data.azurerm_monitor_diagnostic_categories.this.log_category_types)
+    content {
+      category = enabled_log.value
+    }
+    
+  }
+
+  metric {
+    category = "AllMetrics"
+  }
+}
