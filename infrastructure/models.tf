38bc057cb4c5b9f29c0486dcade37e13d0aaf19d
briandenicola <brian@briandenicola.com>
2025-08-11 18:00:14 -0500

    🎮 Mon Aug 11 18:00:14 CDT 2025

--- infrastructure/models.tf
+++ infrastructure/models.tf
@@ -4,26 +4,11 @@ resource "azurerm_cognitive_deployment" "gpt" {
   model {
     format  = "OpenAI"
     name    = "gpt-4o"
-    version = "2024-05-13"
+    version = "2024-11-20"
   }
 
   sku {
     name     = "GlobalStandard"
-    capacity = 10
-  }
-}
-
-resource "azurerm_cognitive_deployment" "gpt4_vision" {
-  name                 = "gpt4-vision"
-  cognitive_account_id = azurerm_cognitive_account.regional.id
-  model {
-    format  = "OpenAI"
-    name    = "gpt-4"
-    version = "vision-preview"
-  }
-
-  sku {
-    name     = "Standard"
     capacity = 10
   }
 }
@@ -38,21 +23,6 @@ resource "azurerm_cognitive_deployment" "text_embedding3_small" {
   }
 
   sku {
-    name = "Standard"
-  }
-}
-
-resource "azurerm_cognitive_deployment" "gpt4_embedding" {
-  name                 = "gpt-4"
-  cognitive_account_id = azurerm_cognitive_account.embedding.id
-  model {
-    format  = "OpenAI"
-    name    = "gpt-4"
-    version = "0613"
-  }
-
-  sku {
-    name     = "Standard"
-    capacity = 10
+    name = "GlobalStandard"
   }
 }
