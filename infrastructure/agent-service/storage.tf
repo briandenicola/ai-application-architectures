1318cf953d9a697bdfde87a00564c2419be3e3d6
briandenicola <brian@briandenicola.com>
2025-12-12 11:48:24 -0600

    🏉 Fri Dec 12 11:48:24 CST 2025

---
+++ infrastructure/agent-service/storage.tf
@@ -0,0 +1,19 @@
+resource "azurerm_storage_account" "this" {
+
+  name                = local.foundry_storage_account_name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+
+  account_kind              = "StorageV2"
+  account_tier              = "Standard"
+  account_replication_type  = "ZRS"
+  shared_access_key_enabled = true
+
+  min_tls_version                 = "TLS1_2"
+  allow_nested_items_to_be_public = false
+  
+  network_rules {
+    default_action = "Allow"
+  }
+}
+
